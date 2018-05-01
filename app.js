6337f055ad4ff794f58abaf4ac26cf2638133130
Gorilla Code <gorillacode.io@gmail.com>
2018-05-01 18:49:58 -0500

    New commit with all files

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function() { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+$(function() {
+	var message = {
+
+		message: [
+			'Frontend Web Developer.',
+			'Creative Designer.',
+			'Movie Geek.',
+      'Cool Father!',
+			'Loving Husband!'
+		],
+		counterS: 0,
+		counterL: 0,
+		deleteS: false,
+
+		init: function() {
+			this.cacheElem();
+			this.type();
+		},
+
+		cacheElem: function() {
+			this.$text = $('.text');
+		},
+
+		type: function() {
+			var message 	= this.message[this.counterS],
+				  that 	    = this,
+				  speed 	  = 0;
+
+			message = !this.deleteS ? message.slice(0, ++this.counterL) : message.slice(0, --this.counterL);
+			if(this.message[this.counterS] != message && !this.deleteS) {
+				this.$text.text(message);
+				speed = 100;
+			}
+			else {
+				this.deleteS = true;
+				speed = this.message[this.counterS] == message ? 1000 : 40;
+				this.$text.text(message);
+				if (message == '') {
+					this.deleteS = false;
+					this.counterS = this.counterS < this.message.length - 1 ? this.counterS + 1 : 0;
+				}
+			}
+			setTimeout(function(){that.type()}, speed);
+		}
+	};
+	message.init();
+});
