e0612620636189e2b77213e540130589f09e3477
Gorilla Code <gorillacode.io@gmail.com>
2018-05-02 20:51:52 -0500

    Touched up code and changed BG

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
 $(function() {
-	var message = {
+  var message = {
 
-		message: [
-			'Frontend Web Developer.',
-			'Creative Designer.',
-			'Movie Geek.',
+    message: [
+      'Frontend Web Developer.',
+      'Creative Designer.',
+      'Movie Geek.',
       'Cool Father!',
-			'Loving Husband!'
-		],
-		counterS: 0,
-		counterL: 0,
-		deleteS: false,
+      'Loving Husband!'
+    ],
+    counterS: 0,
+    counterL: 0,
+    deleteS: false,
 
-		init: function() {
-			this.cacheElem();
-			this.type();
-		},
+    init: function() {
+      this.cacheElem();
+      this.type();
+    },
 
-		cacheElem: function() {
-			this.$text = $('.text');
-		},
+    cacheElem: function() {
+      this.$text = $('.text');
+    },
 
-		type: function() {
-			var message 	= this.message[this.counterS],
-				  that 	    = this,
-				  speed 	  = 0;
+    type: function() {
+      var message = this.message[this.counterS],
+        that = this,
+        speed = 0;
 
-			message = !this.deleteS ? message.slice(0, ++this.counterL) : message.slice(0, --this.counterL);
-			if(this.message[this.counterS] != message && !this.deleteS) {
-				this.$text.text(message);
-				speed = 100;
-			}
-			else {
-				this.deleteS = true;
-				speed = this.message[this.counterS] == message ? 1000 : 40;
-				this.$text.text(message);
-				if (message == '') {
-					this.deleteS = false;
-					this.counterS = this.counterS < this.message.length - 1 ? this.counterS + 1 : 0;
-				}
-			}
-			setTimeout(function(){that.type()}, speed);
-		}
-	};
-	message.init();
+      message = !this.deleteS ? message.slice(0, ++this.counterL) : message.slice(0, --this.counterL);
+      if (this.message[this.counterS] != message && !this.deleteS) {
+        this.$text.text(message);
+        speed = 100;
+      } else {
+        this.deleteS = true;
+        speed = this.message[this.counterS] == message ? 1000 : 40;
+        this.$text.text(message);
+        if (message == '') {
+          this.deleteS = false;
+          this.counterS = this.counterS < this.message.length - 1 ? this.counterS + 1 : 0;
+        }
+      }
+      setTimeout(function() {
+        that.type()
+      }, speed);
+    }
+  };
+  message.init();
 });
